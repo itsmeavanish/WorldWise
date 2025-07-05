557658069d269800912fa3db59a0a566ea91ea0e
Avanish Upadhyay <avanishupadhyay633.com>
2025-07-05 13:58:53 +0530

    Firebase with Gemini things done along with show of trip plan

--- frontend/src/pages/Trip.jsx
+++ frontend/src/pages/Trip.jsx
@@ -1,8 +1,8 @@
 import React from 'react'
 import PageNav from '../components/PageNav'
 import { useCities } from '../contexts/CitiesContext'
-import Tripfetch from '../Trip-Fetching-Firebase/tripfetch';
 import "./Trip.css"
+import Tripdetails from '../Trip-Fetching-Firebase/TripShow';
 export default function Trip() {
     const {currentcity}=useCities();
 
@@ -11,7 +11,7 @@ export default function Trip() {
         <PageNav />
     <div className='trip'>
       <h1>{currentcity.cityName} Travel Experience</h1>
-      <Tripfetch />
+      <Tripdetails />
     </div>
 
     </div>
